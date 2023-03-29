ebf3c501bab1fac1a32139b97b36291b918cdc5d
CoffeeGeek101 <workwithshoumya@gmail.com>
2023-03-29 10:49:52 +0530

    full responsive carousel

--- src/CaroImg.ts
+++ src/CaroImg.ts
@@ -1,12 +1,53 @@
 export const Image = [
     {
         link : 'https://images.unsplash.com/photo-1679855315528-106bb5cea34e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80',
-        alt : 'Arizon'
+        country : 'America',
+        place : 'Utah',
+        frequency : '231'
     },{
         link : 'https://images.unsplash.com/photo-1679926398477-c35c89ba1da7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=985&q=80',
-        alt : 'Spain'
+        country : 'Spain',
+        place : 'Bilbao',
+        frequency : '78'
     },{
         link : 'https://images.unsplash.com/photo-1679834780326-f3e8b5a5a01e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80',
-        alt : 'Somewhere'
-    },
+        country : 'Canada',
+        place : 'Barkervilla',
+        frequency : '145'
+    },{
+        link : 'https://images.unsplash.com/photo-1611051489439-99f01230788d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80',
+        country : 'India',
+        place : 'Jammu and Kashmir',
+        frequency : '456'
+    },{
+        link:'https://images.unsplash.com/photo-1522547902298-51566e4fb383?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1035&q=80',
+        country : 'Japan',
+        place : 'Sensō ji',
+        frequency : '467'
+    },{
+        link:'https://images.unsplash.com/photo-1559628233-100c798642d4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1035&q=80',
+        country : 'Indonesia',
+        place : 'Bali',
+        frequency : '500+'
+    },{
+        link : 'https://images.unsplash.com/photo-1623345805780-8f01f714e65f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80',
+        country : 'Germany',
+        place : 'Rothenburg',
+        frequency : '285'
+    },{
+        link : 'https://images.unsplash.com/photo-1564594736624-def7a10ab047?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80',
+        country : 'France',
+        place : 'Paris',
+        frequency : '421'
+    },{
+        link : 'https://images.unsplash.com/photo-1518780664697-55e3ad937233?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1065&q=80',
+        country : 'Norway',
+        place : 'Westfjords Region',
+        frequency : '89'
+    },{
+        link : 'https://images.unsplash.com/photo-1511135570219-bbad9a02f103?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80',
+        country : 'Italy',
+        place : 'Venezia',
+        frequency : '123'
+    }
 ];
